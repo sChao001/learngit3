c1110b357b56488e9d2f55701c4e04901d9352a0
sChao001 <948994708@qq.com>
2017-04-20 20:19:50 +0800

    learngit3 first commit

---
+++ aaaaaaaaa/aaaaaaaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaaaaaaa
+//
+//  Created by BCH on 2017/4/20.
+//  Copyright © 2017年 BCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaaaaaaa/aaaaaaaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaaaaaa
+//
+//  Created by BCH on 2017/4/20.
+//  Copyright © 2017年 BCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
